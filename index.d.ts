cc494297ee812be47dd1f4a3c3061ea0f9404718
AceLikesGhosts <acelikesghosts@gmail.com>
2022-10-11 19:10:09 -0700

    Proper types!

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,14 @@
-declare function _exports(subdomain: string, fn: any): (req: any, res: any, next: any) => any;
-export = _exports;
+import type { Request, Response } from 'express';
+
+/**
+ * @description The subdomain function.
+ * @param subdomain The subdomain to listen on.
+ * @param fn The listener function, takes a response and request.
+ * @returns A function call to the value passed as FN, or void (the next function).
+ */
+declare function subdomain(
+    subdomain: string,
+    fn: (req: Request, res: Response) => void | any
+): (req: Request, res: Response, next: () => void) => void | typeof fn;
+
+export = subdomain;
